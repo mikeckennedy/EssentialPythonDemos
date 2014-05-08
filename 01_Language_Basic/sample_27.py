3927d60580e8d9ee1ab2e701ad8532352efc0837
Michael Kennedy <mikeckennedy@gmail.com>
2014-05-07 17:54:07 -0700

    minor edits, etc.

--- 01_Language_Basic/sample_27.py
+++ 01_Language_Basic/sample_27.py
@@ -1,5 +1,26 @@
 # import sys
+#
 # print sys.version
 #
 # with file('something.txt', 'w'):
 #     print 'opened the file!'
+#
+#
+# def s():
+#     yield 1
+#     yield 2
+#
+#
+# for i in s():
+#     print i
+#
+# n = {1, 2, 2, 2, 3, 5, 77, 88}
+# e = (
+#     x*x
+#     for x in n
+#     if x % 2 == 0
+# )
+#
+# print(e)
+# for num in e:
+#     print num
