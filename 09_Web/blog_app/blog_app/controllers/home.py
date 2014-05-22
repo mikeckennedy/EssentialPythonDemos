17ed72fc15dc1d640228ab0a27206aa255d516f2
Michael Kennedy <mikeckennedy@gmail.com>
2014-05-22 10:42:01 +0100

    web app shows home page.

--- 09_Web/blog_app/blog_app/controllers/home.py
+++ 09_Web/blog_app/blog_app/controllers/home.py
@@ -4,7 +4,6 @@ from blog_app.controllers.base import BaseController
 
 
 class HomeController(BaseController):
-    @pyramid_handlers.action()
+    @pyramid_handlers.action(renderer="blog_app:templates/home/index.pt")
     def index(self):
-        return pyramid.response.Response("index")
-
+        return {'project': "Blog app"}
--- 09_Web/blog_app/blog_app/controllers/posts.py
+++ 09_Web/blog_app/blog_app/controllers/posts.py
@@ -1,14 +1,16 @@
 import datetime
+
 import pyramid.httpexceptions
 import pyramid.response
-from blog_app.controllers.base import BaseController
 import pyramid_handlers
+
+from blog_app.controllers.base import BaseController
 import blog_app.data.data_layer
 from blog_app.data.data_layer import Post
 from blog_app.data.data_layer import DB
 
-class PostController(BaseController):
 
+class PostController(BaseController):
     @pyramid_handlers.action(renderer="blog_app:templates/blog/list.pt")
     def list(self):
         ordered_posts = blog_app.data.data_layer.DB.all_posts
@@ -19,7 +21,7 @@ class PostController(BaseController):
     def show(self):
         the_id = int(self.data['id'])
         post = blog_app.data.data_layer.DB.find_post_by_id(the_id)
-        return pyramid.response.Response("would show " + post.title )
+        return pyramid.response.Response("would show " + post.title)
 
     @pyramid_handlers.action(
         name='create',
@@ -37,7 +39,7 @@ class PostController(BaseController):
         pub = datetime.datetime.now()
         content = self.data['content']
         title = self.data['title']
-        p =Post(4, title, pub, content)
+        p = Post(4, title, pub, content)
         DB.all_posts.append(p)
 
         return pyramid.httpexceptions.HTTPFound('/posts')
