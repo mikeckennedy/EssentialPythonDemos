d443863a77fe9198ced20f15b361fe6525cedcb7
Michael Kennedy <mikeckennedy@gmail.com>
2014-06-28 07:14:33 -0700

    Cleanup.

--- 01_Intro_to_python/lib.py
+++ 01_Intro_to_python/lib.py
@@ -7,3 +7,16 @@ if __name__ == "__main__":
     print('Calling hi once to set things up...')
     say_hi()
 
+
+
+
+# x = None # == null or NULL
+# y = False
+# print(type(y))
+#
+# if not x:
+#     print("x is true")
+#
+# if not y:
+#     print("y is true")
+#
